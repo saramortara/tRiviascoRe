7821f79b5a0c64c4ec3d8afe5354346e40fb3830
saramortara <saramortara@gmail.com>
2021-07-08 22:07:03 -0300

    fix typo and order

--- R/rank_teams.R
+++ R/rank_teams.R
@@ -1,6 +1,6 @@
 #' Classify tRivia teams based on score
 #'
-#' @param res data frame containing at least two colums: `team`, `score` and `round` (optional)
+#' @param res data frame containing at the colums: `timestamp`, `team`, `score`, and `round` returned by the score_trivia function
 #'
 #' @export
 #'
@@ -10,15 +10,21 @@
 
 rank_teams <- function(res){
 
+  # Colors for messages
   info <- combine_ansi_styles("darkorchid", "bold")
   note <- combine_ansi_styles("darkgreen")
 
+  # Calculating sum of scores
   score <- aggregate(score ~ team, data = res, FUN = sum, na.rm = TRUE)
 
+  # Adding timestamp ad included columns to help ranking
   score$timestamp = res$timestamp[res$round == max(res$round)]
 
-  score$order = -(score$score + 1)
+  # Removing responses not included
+  if (nrow(score) == 0) {stop("No team made to the final!")}
 
+  # Ordering teams
+  score$order <- -(score$score + 1)
   rank <- score[order(score$order, score$timestamp), ]
 
 
@@ -40,6 +46,6 @@ rank_teams <- function(res){
 
   message(info("Third place: ", third$team), note(" Score: ", third$score, " "), medal(3), ifelse(third$tie, "Tiebreaker by timestamp", ""))
   message(info("Second place: ", second$team), note(" Score: ", second$score, " "), medal(2), ifelse(second$tie, "Tiebreaker by timestamp", ""))
-  message(info("First place: ", first$team), note(" Score: ", first$score, " "), medal(1), ifelse(second$tie, "Tiebreaker by timestamp", ""))
+  message(info("First place: ", first$team), note(" Score: ", first$score, " "), medal(1), ifelse(first$tie, "Tiebreaker by timestamp", ""))
 
 }
--- R/score_trivia.R
+++ R/score_trivia.R
@@ -4,6 +4,8 @@
 #' @param n_round vector indicating the round of questions, if applies
 #' @param score Numeric value indicating the score for questions at the round
 #' @param ref Data frame containing two columns: 'question_name' and 'correct_answer'
+#' @param start Data character from when to start taking responses into account in dmy_hms format
+#' @param end Data character from when to end taking responses into account in dmy_hms format
 #'
 #' @export
 #'
@@ -14,16 +16,50 @@
 #' @importFrom magrittr %>%
 #' @importFrom gsheet construct_download_url
 #'
-score_trivia <- function(data, n_round = 1, score = 2, ref, time_col = 1, team_col = 2){
+score_trivia <- function(data,
+                         n_round = 1,
+                         score = 2,
+                         ref,
+                         time_col = 1,
+                         team_col = 2,
+                         start = NULL,
+                         end = NULL){
 
   if (is.character(data)) data <- read_csv(construct_download_url(data))
 
+  # Setting better variable names
   names(data)[time_col] <- "timestamp"
   names(data)[team_col] <- "team_name"
 
   # Removing empty responses
   data <- data[!is.na(data[, time_col]), ]
   data <- data[!is.na(data[, team_col]), ]
+
+  # Converting timestamp into data format
+  data$timestamp <- dmy_hms(data$timestamp)
+
+  # Setting NA to responses that won't be taken into account
+  # No responses before start date
+  if (!is.null(start)) {
+    start <- dmy_hms(start)
+    for (i in 1:nrow(data)) {
+      if (data$timestamp[i] < start) {
+        data[i, c(-team_col, -time_col)] <- NA
+        #data$included <- FALSE
+      }
+    }
+  }
+
+  # No responses after end date
+  if (!is.null(end)) {
+    end <- dmy_hms(end)
+    for (i in 1:nrow(data)) {
+      if (data$timestamp[i] > end) {
+        data[i, c(-team_col, -time_col)] <- NA
+        #data$included <- FALSE
+      }
+    }
+  }
 
   # Checking if there's only one form per team
   ## if more than one, we'll pick the
@@ -38,9 +74,9 @@ score_trivia <- function(data, n_round = 1, score = 2, ref, time_col = 1, team_c
   # Checking if answer matches responses ---------------------------------------
 
   #if (!is.null(n_round)) {
-    ref_round <- ref %>%
-      filter(.data$round == n_round) %>%
-      select("question_name", "correct_answer")
+  ref_round <- ref %>%
+    filter(.data$round == n_round) %>%
+    select("question_name", "correct_answer")
   #}
 
   quest <- data[, ref_round$question_name]
@@ -56,7 +92,10 @@ score_trivia <- function(data, n_round = 1, score = 2, ref, time_col = 1, team_c
 
   }
 
-  res <- data.frame(timestamp = data$timestamp, team = data$team_name, score = rowSums(quest_res, na.rm = TRUE), round = n_round)
+  res <- data.frame(timestamp = data$timestamp,
+                    team = data$team_name,
+                    score = rowSums(quest_res, na.rm = TRUE),
+                    round = n_round)
 
   return(res)
 
